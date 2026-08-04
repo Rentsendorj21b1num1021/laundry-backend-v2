e75aea4ce048796c84a6332a6b404b3d0a0c8751
kaze <rentsendorj@mezorn.com>
2026-08-04 17:45:42 +0800

    minor bug fix

--- routes/bo-api/index.js
+++ routes/bo-api/index.js
@@ -2,11 +2,12 @@ import express from 'express'
 
 import authRoutes from './auth/authRoutes.js'
 import merchantRoutes from './merchant/index.js'
+import userRoutes from './user/index.js'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
 router.use('/merchant', merchantRoutes)
-router.use('/user', merchantRoutes)
+router.use('/user', userRoutes)
 
 export default router
