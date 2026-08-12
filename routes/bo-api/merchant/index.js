9c8f42dcbf46818a5531711451e579cd7526479b
reired63 <reired63@gmail.com>
2026-08-12 23:23:18 +0800

    fix: lint error

--- routes/bo-api/merchant/index.js
+++ routes/bo-api/merchant/index.js
@@ -1,14 +1,14 @@
-import express from "express";
+import express from 'express'
 
-import createHandler from "../../../controllers/bo-api/merchant/create.js";
-import detailHandler from "../../../controllers/bo-api/merchant/detail.js";
-import listHandler from "../../../controllers/bo-api/merchant/list.js";
-import { auth, superAdminOnly } from "../../../middleware/authMiddleware.js";
+import createHandler from '../../../controllers/bo-api/merchant/create.js'
+import detailHandler from '../../../controllers/bo-api/merchant/detail.js'
+import listHandler from '../../../controllers/bo-api/merchant/list.js'
+import { auth, superAdminOnly } from '../../../middleware/authMiddleware.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/list", auth, superAdminOnly, listHandler);
-router.post("/create", auth, superAdminOnly, createHandler);
-router.post("/detail", auth, superAdminOnly, detailHandler);
+router.post('/list', auth, superAdminOnly, listHandler)
+router.post('/create', auth, superAdminOnly, createHandler)
+router.post('/detail', auth, superAdminOnly, detailHandler)
 
-export default router;
+export default router
